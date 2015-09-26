8c82bd54cb877b04daff6d59de61071a5c7d0ce0
SilverWingedSeraph <evessarudem@gmail.com>
2015-09-26 15:17:54 -0700

    Add init_db() which overwrites the database, creating it if it does not exist.

--- silvr.py
+++ silvr.py
@@ -2,9 +2,18 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
+from contextlib import closing # For init.ing database
 
 app = Flask(__name__)
 app.config.from_object('config') # Import configuration data
+
+def init_db():
+    with closing(connect_db()) as database: # Closes the connection once the following work is done
+        with app.open_resource(app.config['SCHEMA'], mode='r') as schema: # Open the app schema for reading
+            # Reads in SQL script and executes it in the database
+            database.cursor().executescript(schema.read())
+        # Commit the database now that the script is done
+        database.commit()
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
