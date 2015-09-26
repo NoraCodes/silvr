6e195b9763adea58a80dad900585cfad6853b802
SilverWingedSeraph <evessarudem@gmail.com>
2015-09-26 15:06:48 -0700

    Add config file (imported using from config import *)

---
+++ config.py
@@ -0,0 +1,7 @@
+# Configuration for silvr blog
+
+DATABASE = 'silvr.db'
+DEBUG = True # Are we in debug mode?
+SECRET_KEY = 'dev key'
+USERNAME = 'admin'
+PASSWORD = 'default' # TODO: Make this not awful
